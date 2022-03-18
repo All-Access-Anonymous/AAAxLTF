c8ee569aac75d3e992a6d7ee5fbbad7508dbf3df
Daniel Ahn <dahn510@gmail.com>
2022-03-18 13:03:34 -0700

    added missing python modules pwlf and plotly

--- main.py
+++ main.py
@@ -2,18 +2,56 @@ from typing import Any
 from timeit import default_timer as timer
 from aaa.sim_handler import SimHandler
 from aaa.logger import pkg_logger as pl
+from aaa.schema import AttendeeConfig, MarketConfig, SimConfig
+from aaa import Config
 
+
+Log = pl.Logger().get_logger()
 
 def test_received_config() -> dict:
-    return {}
+    # This is indeed a dictionary
+
+    print(Config.sim_confs)
+    return Config.sim_confs
 
 def run_sim(configs: dict = {}) -> Any:
 
-    #    start = timer()
-    conf_to_submit: dict = configs | test_received_config()
-    sim = SimHandler(conf_to_submit)
+    conf_to_submit: dict = configs 
+    validated_conf = SimConfig(**conf_to_submit)
 
-    sim.run()
+    sim = SimHandler(conf_to_submit)
+    return sim.run()
+
+
 
 if __name__ == "__main__":
-    run_sim()
+
+    start = timer()
+    # Test config
+    config={
+      "days": 31,
+      "buy_day_weights": [
+        0.2,
+        0.3,
+        0.2,
+        0.2,
+        0.1
+      ],
+      "attendee_count": 200,
+      "attendee_config": {
+        "lateness": [
+          0.5,
+          0.9
+        ]
+      },
+      "market_config": {
+        "price_increase_multiplier": 1.05,
+        "log_day": True
+      },
+      "seating_levels": 5
+    }
+    sim_res: dict = run_sim(config)
+    end = timer()
+    Log.info(f'Simulation time: {end - start}')
+    print(sim_res)
+
